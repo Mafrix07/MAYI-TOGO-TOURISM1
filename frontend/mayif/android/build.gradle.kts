aa433f6d5b3d6cab559ced2ed65c32b3d2acec19
Mafrix07 <dalmeidamario7@gmail.com>
2026-03-23 13:07:19 +0000

    feat: remplacement du frontend Flutter par la version de origin/master

--- frontend/mayif/android/build.gradle.kts
+++ frontend/mayif/android/build.gradle.kts
@@ -5,34 +5,16 @@ allprojects {
     }
 }
 
-// Fix pour l'erreur "different roots" entre le disque C: et D:
-val rootBuildDir = rootProject.layout.projectDirectory.dir("../build")
-rootProject.layout.buildDirectory.set(rootBuildDir)
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val subprojectDir = project.projectDir.absolutePath
-    val rootDir = rootProject.projectDir.absolutePath
-
-    // Si le sous-projet est sur le même disque que le projet racine (ex: D:), on redirige le buildDir
-    // vers le dossier build à la racine du projet Flutter (D:\...\build)
-    if (subprojectDir.take(1).equals(rootDir.take(1), ignoreCase = true)) {
-        project.layout.buildDirectory.set(rootBuildDir.dir(project.name))
-    }
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
-// Désactivation des tests unitaires pour les plugins (évite l'erreur de disque et accélère le build)
-subprojects {
-    afterEvaluate {
-        if (project.name != "app") {
-            tasks.forEach { task ->
-                if (task.name.contains("UnitTest")) {
-                    task.enabled = false
-                }
-            }
-        }
-    }
-}
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
